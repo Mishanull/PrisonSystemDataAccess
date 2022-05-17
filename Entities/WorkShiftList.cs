64a928b5de2891bdf6a5134016f3cf4c31cbb24c
Juraj Sabik <jsb@trifork.com>
2022-05-18 01:16:29 +0200

    getting workshift by id

---
+++ Entities/WorkShiftList.cs
@@ -0,0 +1,17 @@
+﻿namespace Entities;
+
+public class WorkShiftList
+{
+    public ICollection<WorkShift> WorkShifts { get; set; }
+
+    public WorkShiftList()
+    {
+        WorkShifts = new List<WorkShift>();
+    }
+
+    public WorkShiftList(ICollection<WorkShift> workShifts)
+    {
+        WorkShifts = workShifts;
+    }
+    
+}
--- Interfaces/IWorkShiftService.cs
+++ Interfaces/IWorkShiftService.cs
@@ -6,6 +6,7 @@ public interface IWorkShiftService
 {
     public Task<WorkShift> CreateWorkShiftAsync(WorkShift shift);
     public Task<ICollection<WorkShift>> GetWorkShiftsAsync();
+    public Task<WorkShift> GetWorkShiftByIdAsync(long id);
     public Task<WorkShift> AddGuardToWorkShiftAsync(long guardId, long shiftId);
     public Task RemoveWorkShiftAsync(long id);
     public Task<WorkShift> UpdateWorkShiftAsync(WorkShift shift);
