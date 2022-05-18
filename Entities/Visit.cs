6bb5267d6599a7caaa2eaae22e3f681ebb537670
Emiliano <emijos3@hotmail.com>
2022-05-18 16:33:04 +0200

    visit null

--- Entities/Visit.cs
+++ Entities/Visit.cs
@@ -3,9 +3,9 @@
 public class Visit
 {
     public long Id { get; set; }
-    public DateTime DateTime { get; set; }
-    public Status Status0 { get; set; }
-    public string AccessCode { get; set; }
+    public DateTime? DateTime { get; set; }
+    public Status? Status0 { get; set; }
+    public string? AccessCode { get; set; }
     public string  FirstName { get; set; }
     public string  LastName { get; set; }
     public string Email { get; set; }
