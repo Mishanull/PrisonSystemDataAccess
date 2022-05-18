af8d9a64465cac98f845350ad23e707338584ae0
Benjamín Bartošík <benjamin.bartosik@gmail.com>
2022-05-18 14:30:31 +0200

    prisoner + sector entities updated

---
+++ DataAccessAPI/Controllers/SectorController.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataAccessAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SectorController :ControllerBase
+{
+    private ISectorService _sectorService;
+
+    public SectorController(ISectorService sectorService)
+    {
+        _sectorService = sectorService;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<SectorList>> GetSectors()
+    {
+        try
+        {
+            ICollection<Sector> sectors = await _sectorService.GetSectorsAsync();
+            SectorList sectorList = new(sectors);
+            return Ok(sectorList);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+}
--- Entities/Sector.cs
+++ Entities/Sector.cs
@@ -7,4 +7,19 @@ public class Sector
 {
     public long Id { get; set; }
     public int Capacity { get; set; }
+    public int OccupiedCells { get; set; }
+    
+    public int FreeCells => Capacity - OccupiedCells;
+
+
+    public Sector(long id, int capacity)
+    {
+        Id = id;
+        Capacity = capacity;
+    }
+
+    public override string ToString()
+    {
+        return $"S-{Id} [{OccupiedCells}/{Capacity}]";
+    }
 }
